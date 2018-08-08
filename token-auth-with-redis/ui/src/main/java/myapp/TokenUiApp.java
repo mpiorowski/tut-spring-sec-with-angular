c9aae3a0e0dfcf8f8b9246aea26987cc75d27ce8
Mateusz Piórowski <mateuszp@lsnova.pl>
2018-08-08 10:09:51 +0200

    new token auth

---
+++ token-auth-with-redis/ui/src/main/java/myapp/TokenUiApp.java
@@ -0,0 +1,11 @@
+package myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenUiApp {
+  public static void main(String[] args) {
+    SpringApplication.run(TokenUiApp.class);
+  }
+}
