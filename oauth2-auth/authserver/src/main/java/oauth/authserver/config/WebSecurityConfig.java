9fa5c7dc5ca857a37fc404a4f3f77e0b034bad2a
Mateusz Piórowski <mateuszp@lsnova.pl>
2018-09-24 12:51:15 +0200

    ITS WORKING

--- oauth2-auth/authserver/src/main/java/oauth/authserver/config/WebSecurityConfig.java
+++ oauth2-auth/authserver/src/main/java/oauth/authserver/config/WebSecurityConfig.java
@@ -1,22 +1,43 @@
 package oauth.authserver.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
+  @Bean
   @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.httpBasic().and().authorizeRequests().anyRequest().authenticated();
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
   }
 
-  //  @Bean
-  //  protected OAuth2RestTemplate oAuth2RestTemplate(
-  //      OAuth2ProtectedResourceDetails resourceDetails, OAuth2ClientContext clientContext
-  //  ) {
-  //    return new OAuth2RestTemplate(resourceDetails, clientContext);
-  //  }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.httpBasic()
+        .and()
+        .requestMatchers()
+        .antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
+        .and()
+        .authorizeRequests()
+        .anyRequest()
+        .authenticated();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("pass")).roles("USER");
+  }
 }
